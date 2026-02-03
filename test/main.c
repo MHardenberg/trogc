7618f49a43ca3246eb35fb3c6d932d73bd3b0f73
Michel Hardenberg <au617623@uni.au.dk>
2026-02-03 20:45:48 +0100

    Linalg matrix mults

--- test/main.c
+++ test/main.c
@@ -157,9 +157,92 @@ void test_linalg_f_vecdScale() {
         }
         TEST(res);
 }
-int test_linalg_f_matdIdx();
+
+void test_linalg_f_matdIdx() {
+        double x[3][2] = {{1, 4}, {2, 5}, {3, 6}}; // column major
+        f_matd m = {.rows = 2, .cols = 3, .x = (double *)x};
+        int res = 0;
+        size_t idx = 0;
+        for (size_t i = 0; i < m.rows; ++i) {
+                for (size_t j = 0; j < m.cols; ++j) {
+                        idx = m.rows * j + i;
+                        // printf("%f %f %d %d %d\n", m.x[idx],
+                        //        *f_matdIdx(&m, i, j), i, j, idx);
+                        if (m.x[idx] != *f_matdIdx(&m, i, j)) {
+                                ++res;
+                        }
+                }
+        }
+
+        // f_matdPrint(&m);
+        TEST(res);
+}
+
+void test_linalg_f_matdMVMul() {
+        int res = 0;
+
+        double mx[3][2] = {{1, 4}, {2, 5}, {3, 6}}; // column major
+        double vx[3] = {7, 8, 9};
+        double yx[2] = {0, 0};
+        double expectedY[2] = {550, 1342};
+
+        f_matd m = {.rows = 2, .cols = 3, .x = (double *)mx};
+        f_vecd v = {.size = 3, .x = vx};
+        f_vecd y = {.size = 2, .x = yx};
+        double alpha = 11;
+        f_matdMVMul(&y, &m, &v, alpha);
+
+        /*
+            f_matdPrint(&m);
+            f_vecdPrint(&v);
+            f_vecdPrint(&y);
+        */
+
+        for (size_t i = 0; i < 2; ++i) {
+                if (expectedY[i] != *f_vecdIdx(&y, i)) {
+                        ++res;
+                }
+        }
+        TEST(res);
+}
+
+void test_linalg_f_matdMMul() {
+        int res = 0;
+
+        double mx[3][2] = {{1, 4}, {2, 5}, {3, 6}}; // column major
+        double nx[2][3] = {{1, 3, 5}, {2, 4, 6}};
+        double yx[2][2] = {{0, 0}, {0, 0}};
+
+        double expectedY[2][2] = {{242, 539}, {308, 704}};
+
+        f_matd m = {.rows = 2, .cols = 3, .x = (double *)mx};
+        f_matd n = {.rows = 3, .cols = 2, .x = (double *)nx};
+        f_matd y = {.rows = 2, .cols = 2, .x = (double *)yx};
+        f_matd expected = {.rows = 2, .cols = 2, .x = (double *)expectedY};
+        double alpha = 11;
+
+        f_matdMMul(&y, alpha, &m, &n);
+
+        /*
+            f_matdPrint(&m);
+            f_matdPrint(&n);
+            f_matdPrint(&y);
+            f_matdPrint(&expected);
+        */
+
+        for (size_t i = 0; i < y.rows; ++i) {
+                for (size_t j = 0; j < y.cols; ++j) {
+                        if (*f_matdIdx(&expected, i, j) !=
+                            *f_matdIdx(&y, i, j)) {
+                                ++res;
+                        }
+                }
+        }
+        TEST(res)
+}
 
 int main() {
+        // vectors
         test_linalg_f_vecdZeros();
         test_linalg_f_vecdOnes();
         test_linalg_f_vecdIdx();
@@ -168,5 +251,10 @@ int main() {
         test_linalg_f_vecdEmul();
         test_linalg_f_vecdMul();
         test_linalg_f_vecdScale();
+
+        // matrices
+        test_linalg_f_matdIdx();
+        test_linalg_f_matdMVMul();
+        test_linalg_f_matdMMul();
         return 0;
 }
