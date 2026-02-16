ac570a46a562bd94efafba70d1fe340cf3522de1
Michel Hardenberg <au617623@uni.au.dk>
2026-02-16 16:11:08 +0100

    updated plotting

---
+++ README.md
@@ -0,0 +1,19 @@
+# Forgelib
+General library primarily for scientific computing in C.
+Desined with custom maths containers and efficient allocator stategies.
+
+## Dependencies
+- CMake
+- OpenBlas and/or CUDA
+
+### Optional
+- gnuplot (if plotting is needed)
+
+## Installation
+```bash
+git clone --recursive git@github.com:MHardenberg/forge.git
+cd forge/bin
+cmake ..
+make
+cd ../../
+```
--- test/main.c
+++ test/main.c
@@ -260,13 +260,24 @@ int main() {
         test_linalg_f_matdMMul();
 
         double x[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-        double y[10];
+        double y0[10];
+        double y1[10];
+        const double *ys[] = {y0, y1};
         for (int i = 0; i < 10; ++i) {
-                y[i] = x[i] * x[i];
+                y0[i] = x[i] * x[i];
+                y1[i] = x[i] * x[i] * x[i];
         }
         f_vecd vx = {.size = 10, .x = x};
-        f_vecd vy = {.size = 10, .x = y};
+        f_vecd vy0 = {.size = 10, .x = y0};
+        f_vecd vy1 = {.size = 10, .x = y1};
 
-        f_plot("Test", &vx, &vy, 1);
+        const f_vecd *vs[2] = {&vy0, &vy1};
+        const char *labels[2] = {"y0", "y1"};
+        LOG(PROJECT_ROOT);
+        f_plotv("Test vector", &vx, &vy0, labels[0]);
+        f_plotvs("Test vectors", &vx, vs, labels, 2);
+
+        f_plota("Test array", vx.x, vy0.x, vx.size, labels[0]);
+        f_plotas("Test arrays", x, ys, vx.size, 2, labels);
         return 0;
 }
